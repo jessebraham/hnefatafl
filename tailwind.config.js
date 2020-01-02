653639a387b6e06e800308ecb37945a17c6a7430
Jesse Braham <jesse@beta7.io>
2020-01-02 12:58:38 -0800

    Minor styling updates

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "gray-300": "#dbdcdc",
+        "gray-400": "#8c8e8f",
+        "gray-600": "#707377",
+        "gray-700": "#414244",
+        "gray-800": "#242526",
+        "gray-900": "#17181a",
+      },
+    },
+  },
+}
